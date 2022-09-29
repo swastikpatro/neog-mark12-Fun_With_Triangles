4e0fe72cad5233a2da237887d468e88b1a3be207
swastikpatro <patroswastik.jsdev@gmail.com>
2022-09-30 00:29:51 +0530

    fix: removed alerts, error-Msg to user on output

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,25 +4,14 @@ const baseInput = document.querySelector('#base-input');
 const heightInput = document.querySelector('#height-input');
 const btnContainer = document.querySelector('.btn-container');
 const output = document.querySelector('.output-section');
-const alertText = document.querySelector('.alert');
-
-function alertMsg(type, msg, ms) {
-  const tID = setInterval(() => {
-    alertText.innerText = msg;
-    alertText.classList.add(`alert-${type}`);
-    alertText.classList.add('show-alert');
-  }, 0);
-
-  setTimeout(() => {
-    clearInterval(tID);
-    alertText.classList.remove(`alert-${type}`);
-    alertText.classList.remove('show-alert');
-  }, ms);
-}
 
 function displayOutput(hypoVal) {
   output.innerHTML = `The length of hypotenuse is 
   <span>${hypoVal}</span>`;
+}
+
+function displayErrorMsgToUser(msg) {
+  output.innerHTML = `<span style="color:red">${msg}</span>`;
 }
 
 function squared(num) {
@@ -42,22 +31,20 @@ function handleContainerClick(e) {
     baseInput.value = '';
     heightInput.value = '';
     output.innerText = '';
-    alertMsg('success', 'Cleared', 1000);
     return;
   }
 
   if (!(baseInput.value && heightInput.value)) {
-    alertMsg('danger', 'Please fill all input fields 🙏', 1000);
+    displayErrorMsgToUser('Please fill all input fields 🙏');
     return;
   }
   if (baseInput.value <= 0 || heightInput.value <= 0) {
-    alertMsg('danger', "Inputs can't be zero or less than zero ❌", 1000);
+    displayErrorMsgToUser("Values can't be zero or less than zero ❌");
     return;
   }
 
   const hypotenuse = Math.sqrt(squared(base) + squared(height)).toFixed(2);
   displayOutput(hypotenuse);
-  alertMsg('success', 'Done ✅', 1000);
 }
 
 btnContainer.addEventListener('click', handleContainerClick);
